d5256d39edcd8e33cc51dfbdfd891b59fc27c8a6
Denny Wu <denny.wu87@gmail.com>
2017-09-24 22:35:53 -0700

    Continuing work on js logic, need to finish code to push train info into database

--- app.js
+++ app.js
@@ -9,3 +9,40 @@ var config = {
 };
 
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var frequency = 0;
+var firstArrival = "";
+var nextArrival = "";
+var minutesAway = 0;
+
+$("#add-train").on("click", function(event) {
+
+	event.preventDefault();
+
+	trainName = $("#name-input").val().trim();
+	destination = $("#place-input").val().trim();
+	frequency = $("#frequency-input").val().trim();
+	firstArrival = $("#time-input").val().trim();
+
+	var trElement = $("<tr>")
+	var tdName = $("<td>" + trainName + "</td>")
+	var tdDestination = $("<td>" + destination + "</td>")
+	var tdFrequency = $("<td>" + frequency + "</td>")
+	
+	trElement.append(tdName)
+	trElement.append(tdDestination)
+	trElement.append(tdFrequency)
+
+	$("#table-body").append(trElement)
+
+	database.ref().push({
+
+		trainName: trainName
+		destination: destination
+		frequency: frequency
+	})
+})
