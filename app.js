71481b5c62bfa9e38a1a6e29fcc82ba0257e1476
Denny Wu <denny.wu87@gmail.com>
2017-09-24 21:44:12 -0700

    Beginning work on js logic

---
+++ app.js
@@ -0,0 +1,11 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyD6aYR5s1JyREQGGj-PzIz2tkKiii3NWBw",
+    authDomain: "train-scheduler-assignme-e9397.firebaseapp.com",
+    databaseURL: "https://train-scheduler-assignme-e9397.firebaseio.com",
+    projectId: "train-scheduler-assignme-e9397",
+    storageBucket: "train-scheduler-assignme-e9397.appspot.com",
+    messagingSenderId: "36786468944"
+};
+
+firebase.initializeApp(config);
