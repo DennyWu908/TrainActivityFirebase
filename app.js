85ee2f4c5056e1b1ec04bc8fb279dc2dc60d8d83
Denny Wu <denny.wu87@gmail.com>
2017-09-24 22:50:23 -0700

    Javascript logic needs to include moment js code

--- app.js
+++ app.js
@@ -41,8 +41,56 @@ $("#add-train").on("click", function(event) {
 
 	database.ref().push({
 
-		trainName: trainName
-		destination: destination
+		trainName: trainName,
+		destination: destination,
 		frequency: frequency
+
 	})
+
+	$("#name-input").val("");
+	$("#place-input").val("");
+	$("#frequency-input").val("");
+	$("#time-input").val("");
+
 })
+
+database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+	var sv = snapshot.val();
+
+	console.log(sv.trainName);
+	console.log(sv.destination);
+	console.log(sv.frequency);
+
+}, function(errorObject) {
+      console.log("Errors handled: " + errorObject.code);
+});
+
+var connectionsRef = database.ref("/connections");
+
+// '.info/connected' is a special location provided by Firebase that is updated every time
+// the client's connection state changes.
+// '.info/connected' is a boolean value, true if the client is connected and false if they are not.
+var connectedRef = database.ref(".info/connected");
+
+// When the client's connection state changes...
+connectedRef.on("value", function(snap) {
+
+  // If they are connected..
+  if (snap.val()) {
+
+    // Add user to the connections list.
+    var con = connectionsRef.push(true);
+
+    // Remove user from the connection list when they disconnect.
+    con.onDisconnect().remove();
+  }
+});
+
+// When first loaded or when the connections list changes...
+connectionsRef.on("value", function(snap) {
+
+  // Display the viewer count in the html.
+  // The number of online users is the number of children in the connections list.
+  $("#admin-count").html("Connected Administrators: " + snap.numChildren());
+});
